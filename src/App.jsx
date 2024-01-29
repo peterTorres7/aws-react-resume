674a43a558c276e02f33fc4fef660ac0072fe64d
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-01-29 08:55:14 -0500

    redid structure

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import Header from './components/Header'
+import Summary from './components/Summary'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Certifications from './components/Certifications'
+import Education from './components/Education'
+
+function App() {
+  return (
+    <>
+      <div className='Header'>
+        <Header />
+      </div>
+      <div className='Summary'>
+        <Summary />
+      </div>
+      <div className='Skills'>
+        <Skills />
+      </div>
+      <div className='Experience'>
+        <Experience />
+      </div>
+      <div className='Certifications'>
+        <Certifications />
+      </div>
+      <div className='Education'>
+        <Education />
+      </div>
+    </>
+  )
+}
+
+export default App
