581784d932e683c1e3fdba3257340282605bd20f
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-04-27 14:02:29 -0400

    cloudfront invalidation test

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -2,7 +2,8 @@ function Certifications() {
     return (
         <div className="left-align">
             <h2>Certifications</h2>
-            <h3>AWS Cloud Certified Practitioner | January 2024</h3>
+            <h3>AWS Certified Solutions Architect | April 2024</h3>
+            <h3>AWS Certified Cloud Practitioner | January 2024</h3>
             <ul>
                 <li>Developed a solid understanding of AWS services such as EC2, S3, and RDS.</li>
                 <li>Gained expertise in fundamental cloud computing concepts and best practices.</li>
