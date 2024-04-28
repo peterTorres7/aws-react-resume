5bb710d3ad66632b8132cb6f0d697f4a34c41f08
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-04-28 14:40:31 -0400

    added aws saa certification

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -3,6 +3,10 @@ function Certifications() {
         <div className="left-align">
             <h2>Certifications</h2>
             <h3>AWS Certified Solutions Architect | April 2024</h3>
+            <ul>
+                <li>Designed and implemented scalable, secure, and cost-effective cloud solutions leveraging AWS services.</li>
+                <li>Demonstrated proficiency in architecting for high availability, fault tolerance, and disaster recovery on the AWS platform.</li>
+            </ul>
             <h3>AWS Certified Cloud Practitioner | January 2024</h3>
             <ul>
                 <li>Developed a solid understanding of AWS services such as EC2, S3, and RDS.</li>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,27 +25,3 @@ function Footer() {
 }
 
 export default Footer;
-
-
-
-
-
-// function Footer() {
-//     var view_count = 10;
-//     async function updateViews() {
-//         let response = await fetch('https://3hp6widwdb.execute-api.us-east-1.amazonaws.com/dev');
-//         view_count = await response.json();
-//         console.log(view_count)
-//         return view_count;
-//     }
-//     // console.log(view_count)
-
-//     updateViews()
-//     return (
-//         <div>
-//             <p>Number of visits: {view_count}.</p>
-//         </div>
-//     )
-// }
-
-// export default Footer;
