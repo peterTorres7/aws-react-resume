e14bcc9cabf3ea646b1cfd81d54f5a766e0f44c6
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-12-01 20:50:30 -0500

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
             <h1>Peter Torres</h1>
             <p>
                 New York, NY | 
-                <a href='mailto:torres7p@gmail.com'> Email</a> | 
-                <a href='https://www.linkedin.com/in/torres-peter' target="_blank" rel="noopener noreferrer"> LinkedIn</a> | 
-                <a href='https://www.github.com/peterTorres7' target='_blank' rel='noopener noreferrer'> GitHub</a>
+                <a href='mailto:torres7p@gmail.com'> torres7p@gmail.com</a> | 
+                <a href='https://www.linkedin.com/in/torres-peter' target="_blank" rel="noopener noreferrer"> linkedin.com/in/torres-peter</a> | 
+                <a href='https://www.github.com/peterTorres7' target='_blank' rel='noopener noreferrer'> github.com/peterTorres7</a>
             </p>
         </div>
     )
