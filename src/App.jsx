a366c1fbb01c5b794bcc286479c25009b6364800
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-12-02 12:11:29 -0500

    added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Summary from './components/Summary'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
+import Projects from './components/Projects'
 import Certifications from './components/Certifications'
 import Education from './components/Education'
 import Footer from './components/Footer'
@@ -21,6 +22,9 @@ function App() {
       </div>
       <div className='Experience'>
         <Experience />
+      </div>
+      <div className='Projects'>
+        <Projects />
       </div>
       <div className='Certifications'>
         <Certifications />
---
+++ src/components/Projects.jsx
@@ -0,0 +1,18 @@
+function Projects() {
+    return (
+        <div className="left-align">
+            <h2>Projects</h2>
+            <h3>AWS Resume Challenge | December 2024</h3>
+            <ul>
+                <li>Built and deployed a serverless web application using S3 for static website hosting, CloudFront for content delivery, 
+                    and Route 53 for custom domain management.
+                </li>
+                <li>Developed scalable and cost-efficient backend services with AWS Lambda, API Gateway, and DynamoDB, 
+                    automating infrastructure provisioning and updates with Terraform for consistent and reproducible deployments.
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Projects;
