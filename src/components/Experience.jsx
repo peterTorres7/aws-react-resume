ca9d6b315ad334bb426edd76ac75b20c47d3c786
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-12-07 20:59:27 -0500

    updated education and fixed spacing

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,35 +1,32 @@
 function Experience() {
     return (
         <div className="left-align">
-            <h2>Experience</h2>
-            <h3>Software Engineer | Capital One | June 2024 - December 2024</h3>
+            <h2 className="bottom-margin">Experience</h2>
+            <h3 className="bottom-margin">Software Engineer | Capital One | June 2024 - December 2024</h3>
             <ul>
                 <li>
                     Diagnosed, documented, and resolved bugs by collaborating with intent owners to confirm business requirements 
                     and ensure the expected user experience using Vue.
                 </li>
-                <li>
+                <li className="bottom-margin">
                     Improved integration test coverage by implementing both happy-path and negative test cases for various workflow variants.
                 </li>
             </ul>
-            <h3>Software Engineer | TD Bank | January 2023 - November 2023</h3>
+            <h3 className="bottom-margin">Software Engineer | TD Bank | January 2023 - November 2023</h3>
             <ul>
                 <li>
-                    Proactively engineered a modular code scanning script that enhanced code quality and swiftly identified vulnerabilities, 
+                    Helped create a modular code scanning script that enhanced code quality and swiftly identified vulnerabilities, 
                     adapting to different project requirements and significantly reducing manual configuration needs using Python and Linux.
                 </li>
-                <li>
-                    Facilitated a GitHub workshop for 20 users by setting up access, repositories, and training materials, streamlining team efforts.
-                </li>
-                <li>
+                <li className="bottom-margin">
                     Adapted quickly to cross-platform requirements by deploying and configuring build tools, 
                     ensuring seamless script compatibility across Windows and Linux environments, which improved the team's ability to work in diverse setups.
                 </li>
             </ul>
-            <h3>Software Engineer Intern | Floop Edu, Inc. | April 2021 - December 2021</h3>
+            <h3 className="bottom-margin">Software Engineer Intern | Floop Edu, Inc. | April 2021 - December 2021</h3>
             <ul>
                 <li>
-                    Proactively engineered and deployed a templating feature that reduced teachers' feedback-sharing time, demonstrating adaptability to meet user needs.
+                    Engineered and deployed a templating feature that reduced teachers' feedback-sharing time, demonstrating adaptability to meet user needs.
                 </li>
                 <li>
                     Collaborated effectively with cross-functional teams to refactor frontend components, resolve bugs, and enhance user experience with HTML, CSS, and React.
